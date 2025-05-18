31350b2804bbab70ce24d44dff3c21d38218ac43
Mqx <62719703+Mqxx@users.noreply.github.com>
2025-05-18 18:59:55 +0200

    debugging

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,38 @@
 import actionsCore from '@actions/core';
 import actionsGithub from '@actions/github';
-// import * as yaml from '@std/yaml'
+import * as yaml from '@std/yaml'
 
 const token = actionsCore.getInput('token');
 const organizationName = actionsCore.getInput('organization-name');
 const onlyPublicRepositories = actionsCore.getBooleanInput('only-public-repositories');
 const labelSearchPattern = actionsCore.getInput('label-search-pattern');
+const repositoryBlacklist = yaml.parse(actionsCore.getInput('repository-blacklist')) as string[];
 const categoriesMap = actionsCore.getInput('categories-map');
 const categoriesDefault = actionsCore.getInput('categories-default');
+
+actionsCore.startGroup('Inputs');
+actionsCore.debug(`organization-name: ${organizationName}`)
+actionsCore.debug(`only-public-repositories: ${onlyPublicRepositories}`)
+actionsCore.debug(`label-search-pattern: ${labelSearchPattern}`)
+actionsCore.debug(`repository-blacklist: ${repositoryBlacklist}`)
+actionsCore.debug(`categories-map: ${categoriesMap}`)
+actionsCore.debug(`categories-default: ${categoriesDefault}`)
+actionsCore.endGroup();
 
 const octokit = actionsGithub.getOctokit(token)
 octokit.rest.repos.listForOrg({org: organizationName}).then((response) => {
   response.data.forEach((repository) => {
     if (onlyPublicRepositories && repository.private) {
-      console.warn(`Ignoring repository "${repository.name}", because it is private and "only-public-repositories" is set to true.`);
+      actionsCore.warning(`Ignoring repository "${repository.name}", because it is private and "only-public-repositories" is set to true.`);
       return;
     }
+    
 
     octokit.rest.issues.listLabelsForRepo({owner: organizationName, repo: repository.name}).then((labels) => {
       const categoryLabel = labels.data.filter((label) => label.name.match(new RegExp(labelSearchPattern)))[0];
       
       if (!categoryLabel) {
-        console.warn(`Ignoring repository "${repository.name}", because it has no category label that matches "${labelSearchPattern}".`);
+        actionsCore.warning(`Ignoring repository "${repository.name}", because it has no category label that matches "${labelSearchPattern}".`);
         return;
       }
       const name = repository.name;
