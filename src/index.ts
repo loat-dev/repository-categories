54d6a2fce7714dc596d8062ffea764b28f81b762
Mqx <62719703+Mqxx@users.noreply.github.com>
2025-05-18 18:50:18 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,14 @@ const token = actionsCore.getInput('token');
 const organizationName = actionsCore.getInput('organization-name');
 const onlyPublicRepositories = actionsCore.getBooleanInput('only-public-repositories');
 const labelSearchPattern = actionsCore.getInput('label-search-pattern');
-const categories = actionsCore.getInput('categories');
+const categoriesMap = actionsCore.getInput('categories-map');
+const categoriesDefault = actionsCore.getInput('categories-default');
 
 const octokit = actionsGithub.getOctokit(token)
 octokit.rest.repos.listForOrg({org: organizationName}).then((response) => {
   response.data.forEach((repository) => {
     if (onlyPublicRepositories && repository.private) {
+      console.warn(`Ignoring repository "${repository.name}", because it is private and "only-public-repositories" is set to true.`);
       return;
     }
 
@@ -19,10 +21,12 @@ octokit.rest.repos.listForOrg({org: organizationName}).then((response) => {
       const categoryLabel = labels.data.filter((label) => label.name.match(new RegExp(labelSearchPattern)))[0];
       
       if (!categoryLabel) {
+        console.warn(`Ignoring repository "${repository.name}", because it has no category label that matches "${labelSearchPattern}".`);
         return;
       }
-
-      console.log(repository.name, categoryLabel.description?.split(',').map((category) => category.trim()));
+      const name = repository.name;
+      const categories = categoryLabel.description?.split(',').map((category) => category.trim()) ?? [];
+      console.log(name, categories);
     })
   })
 })
