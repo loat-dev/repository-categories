5f40791903f3f75ef3bbc10feebd7a9634766080
Mqx <62719703+Mqxx@users.noreply.github.com>
2025-05-18 19:17:43 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -23,34 +23,35 @@ if (actionsCore.isDebug()) {
 const octokit = actionsGithub.getOctokit(token)
 octokit.rest.repos.listForOrg({org: organizationName}).then((response) => {
   response.data.forEach((repository) => {
-    actionsCore.startGroup(`Repository ${repository.name}`);
-
-    if (onlyPublicRepositories && repository.private) {
-      actionsCore.warning(
-        `Ignoring repository "${repository.name}", because it is private and "only-public-repositories" is set to true.`,
-        {title: 'Private repository'}
-      );
-      actionsCore.endGroup();
-      return;
-    }
-    
-
-    octokit.rest.issues.listLabelsForRepo({owner: organizationName, repo: repository.name}).then((labels) => {
-      const categoryLabel = labels.data.filter((label) => label.name.match(new RegExp(labelSearchPattern)))[0];
+    actionsCore.group(
+      `Repository ${repository.name}`,
+      async () => {
+        if (onlyPublicRepositories && repository.private) {
+          actionsCore.warning(
+            `Ignoring repository "${repository.name}", because it is private and "only-public-repositories" is set to true.`,
+            {title: 'Private repository'}
+          );
+          actionsCore.endGroup();
+          return;
+        }
       
-      if (!categoryLabel) {
-        actionsCore.info(
-          `Ignoring repository "${repository.name}", because it has no category label that matches "${labelSearchPattern}".`,
-          
-        );
-        actionsCore.endGroup();
-        return;
-      }
-      const name = repository.name;
-      const categories = categoryLabel.description?.split(',').map((category) => category.trim()) ?? [];
-      console.log(name, categories);
-    })
 
-    actionsCore.endGroup();
+        octokit.rest.issues.listLabelsForRepo({owner: organizationName, repo: repository.name}).then((labels) => {
+          const categoryLabel = labels.data.filter((label) => label.name.match(new RegExp(labelSearchPattern)))[0];
+          
+          if (!categoryLabel) {
+            actionsCore.info(
+              `Ignoring repository "${repository.name}", because it has no category label that matches "${labelSearchPattern}".`,
+              
+            );
+            actionsCore.endGroup();
+            return;
+          }
+          const name = repository.name;
+          const categories = categoryLabel.description?.split(',').map((category) => category.trim()) ?? [];
+          console.log(name, categories);
+        })
+      }
+    );
   })
 })
