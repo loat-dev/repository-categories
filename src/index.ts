e6891b87236e7320a5f0fe83af96a23cf02ee14d
Mqx <62719703+Mqxx@users.noreply.github.com>
2025-05-18 17:51:03 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,26 +4,12 @@ import actionsGithub from '@actions/github';
 
 const token = actionsCore.getInput('token');
 const organizationName = actionsCore.getInput('organization-name');
-const onlyPublicRepositories = actionsCore.getBooleanInput('only-public-repositories');
-const labelSearchPattern = actionsCore.getInput('label-search-pattern');
-const categories = actionsCore.getInput('categories');
+// const onlyPublicRepositories = actionsCore.getBooleanInput('only-public-repositories');
+// const labelSearchPattern = actionsCore.getInput('label-search-pattern');
+// const categories = actionsCore.getInput('categories');
 
 const octokit = actionsGithub.getOctokit(token)
 octokit.rest.repos.listForOrg({org: organizationName}).then((response) => {
-  const repositories = response.data.filter((repository) => {
-    return onlyPublicRepositories ? repository.visibility === 'public' : true
-  })
-  const labels = repositories.map((repository) => {
-    return octokit.rest.issues.listLabelsForRepo({owner: organizationName, repo: repository.name}).then((response) => {
-      const labels = response.data.filter((label) => {
-        return label.name.match(new RegExp(labelSearchPattern))
-      })
-      return labels
-    })
-  })
-  console.log(labels);
+  console.log(response.data);
   
-  Promise.all(labels).then((labels) => {
-    actionsCore.setOutput('labels', JSON.stringify(labels))
-  })
 })
