f7155d81585fc72971acbef297bb7a7ea8607194
Mqx <62719703+Mqxx@users.noreply.github.com>
2025-05-18 18:09:00 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,20 @@ import actionsGithub from '@actions/github';
 
 const token = actionsCore.getInput('token');
 const organizationName = actionsCore.getInput('organization-name');
-// const onlyPublicRepositories = actionsCore.getBooleanInput('only-public-repositories');
-// const labelSearchPattern = actionsCore.getInput('label-search-pattern');
-// const categories = actionsCore.getInput('categories');
+const onlyPublicRepositories = actionsCore.getBooleanInput('only-public-repositories');
+const labelSearchPattern = actionsCore.getInput('label-search-pattern');
+const categories = actionsCore.getInput('categories');
 
 const octokit = actionsGithub.getOctokit(token)
 octokit.rest.repos.listForOrg({org: organizationName}).then((response) => {
-  console.log(response.data);
-  
+  response.data.forEach((repo) => {
+    if (onlyPublicRepositories && repo.private) {
+      return;
+    }
+
+    octokit.rest.issues.listLabelsForRepo({owner: organizationName, repo: repo.name}).then((label) => {
+      console.log(label);
+      
+    })
+  })
 })
