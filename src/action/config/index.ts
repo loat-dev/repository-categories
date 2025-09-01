83026906cfe71abe556aaf875d4a2b2b4471a7d5
Mqxx <62719703+Mqxx@users.noreply.github.com>
2025-09-01 14:40:01 +0200

    Update index.ts

--- src/action/config/index.ts
+++ src/action/config/index.ts
@@ -1,7 +1,8 @@
+export * as file from './file/index.ts'
+export * as input from './input/index.ts'
+
+export { defaults } from './defaults.ts';
+
 export { type Config } from './config.ts';
 export { type TemplateFiles } from './template_files.ts';
 export { type Categories } from './categories.ts';
-export { defaults } from './defaults.ts';
-
-export * as file from './file/index.ts'
-export * as input from './input/index.ts'
