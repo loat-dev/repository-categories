81c4188628ebc2712ce1f6a48f67ca29b9c76bce
Mqxx <62719703+Mqxx@users.noreply.github.com>
2025-09-02 11:14:30 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -23,58 +23,58 @@ const octokit = actionsGithub.getOctokit(token);
 
 octokit.rest.repos.listForOrg({org: config.organizationName}).then(async (response) => {
   
-  actionsCore.info(`Chekcing repositories: ${response.data.map((repository) => repository.name).join(', ')}`)
-  
+  actionsCore.info(`Checking repositories: ${response.data.map((repository) => repository.name).join(', ')}`)
+
   for (const repository of response.data) {
     actionsCore.startGroup(`Processing repository "${repository.name}"...`);
 
-    if (config.onlyPublicRepositories && repository.private) {
-      actionsCore.warning(
-        `Ignoring repository "${repository.name}", because it's private and "only-public-repositories" is set to true.`,
-        {title: 'Private repository'}
-      );
-      actionsCore.endGroup();
-      return;
-    }
+    // if (config.onlyPublicRepositories && repository.private) {
+    //   actionsCore.warning(
+    //     `Ignoring repository "${repository.name}", because it's private and "only-public-repositories" is set to true.`,
+    //     {title: 'Private repository'}
+    //   );
+    //   actionsCore.endGroup();
+    //   return;
+    // }
 
-    if (config.repositoryBlacklist.includes(repository.name)) {
-      actionsCore.warning(
-        `Ignoring repository "${repository.name}", because it's on the repository blacklist.`,
-        {title: 'Repository blacklist'}
-      );
-      actionsCore.endGroup();
-      return;
-    }
+    // if (config.repositoryBlacklist.includes(repository.name)) {
+    //   actionsCore.warning(
+    //     `Ignoring repository "${repository.name}", because it's on the repository blacklist.`,
+    //     {title: 'Repository blacklist'}
+    //   );
+    //   actionsCore.endGroup();
+    //   return;
+    // }
 
-    actionsCore.info('Searching for labels in the repository...');
+    // actionsCore.info('Searching for labels in the repository...');
     
-    await octokit.rest.issues.listLabelsForRepo({owner: config.organizationName, repo: repository.name}).then((response) => {
-      const categoryLabel = response.data.filter((label) => config.labelSearchPattern.test(label.name))[0];
+    // await octokit.rest.issues.listLabelsForRepo({owner: config.organizationName, repo: repository.name}).then((response) => {
+    //   const categoryLabel = response.data.filter((label) => config.labelSearchPattern.test(label.name))[0];
       
-      if (!categoryLabel) {
-        actionsCore.warning(
-          `Ignoring repository "${repository.name}", because it has no category label that matches "${config.labelSearchPattern.source}".`,
-          {title: 'No pattern match'}
-        );
-        return;
-      }
+    //   if (!categoryLabel) {
+    //     actionsCore.warning(
+    //       `Ignoring repository "${repository.name}", because it has no category label that matches "${config.labelSearchPattern.source}".`,
+    //       {title: 'No pattern match'}
+    //     );
+    //     return;
+    //   }
 
-      actionsCore.info(`Found label: ${JSON.stringify(categoryLabel)}`)
+    //   actionsCore.info(`Found label: ${JSON.stringify(categoryLabel)}`)
 
-      // const name = repository.name;
-      // const categories = categoryLabel.description?.split(',').map((category) => category.trim()) ?? [];
+    //   // const name = repository.name;
+    //   // const categories = categoryLabel.description?.split(',').map((category) => category.trim()) ?? [];
 
-      // if (categories.length === 0) {
-      //   actionsCore.warning(
-      //     `Ignoring repository "${repository.name}", because it has no categories.`,
-      //     {title: 'No categories provided'}
-      //   );
-      //   return;
-      // }
+    //   // if (categories.length === 0) {
+    //   //   actionsCore.warning(
+    //   //     `Ignoring repository "${repository.name}", because it has no categories.`,
+    //   //     {title: 'No categories provided'}
+    //   //   );
+    //   //   return;
+    //   // }
 
-      // actionsCore.info(name)
-      // actionsCore.info(categories.join(', '))
-    })
+    //   // actionsCore.info(name)
+    //   // actionsCore.info(categories.join(', '))
+    // })
 
     actionsCore.endGroup();
   }
