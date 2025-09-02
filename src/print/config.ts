1eb4043a69452145cc3f2e25186d756c3d3941f9
Mqxx <62719703+Mqxx@users.noreply.github.com>
2025-09-02 11:48:47 +0200

    Update config.ts

--- src/print/config.ts
+++ src/print/config.ts
@@ -1,10 +1,8 @@
 import * as actionsCore from '@actions/core'
-import * as colors from "@std/fmt/colors";
-
 import * as action from '../action/index.ts';
 
 export function config(config: action.config.Config) : void {
-  actionsCore.startGroup(colors.gray('Config:'))
+  actionsCore.startGroup('Config:')
   actionsCore.info(Deno.inspect(config, {colors: true, depth: Infinity}))
   actionsCore.endGroup()
 }
