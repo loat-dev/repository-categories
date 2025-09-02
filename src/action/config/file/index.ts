cda4445b3e8ede67dc58b89a3d60b5a134ba9a42
Mqxx <62719703+Mqxx@users.noreply.github.com>
2025-09-02 09:09:38 +0200

    Update index.ts

--- src/action/config/file/index.ts
+++ src/action/config/file/index.ts
@@ -3,4 +3,3 @@ export * as errors from './errors/index.ts'
 export { read } from './read.ts'
 
 export { type RawConfig } from './raw_config.ts'
-export { type TemplateFiles } from './template_files.ts'
