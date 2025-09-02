664d5022fc4bd31b1c2cdfb185c875d02ca0d8a6
Mqxx <62719703+Mqxx@users.noreply.github.com>
2025-09-02 09:17:33 +0200

    Add organizationName to config schema and usage
    Introduces the organizationName property to config.schema.json and updates the Config interface and getConfig function to support it. This allows specifying which organization to use in the configuration.

--- src/action/config/config.ts
+++ src/action/config/config.ts
@@ -1,11 +1,26 @@
 import { Categories } from './categories.ts';
 import { TemplateFiles } from './template_files.ts';
 
+/**
+ * This interface represents the config for the action.
+ */
 export interface Config {
   organizationName? : string,
+  
+  /** If only public repositories should be queried. Private repositories will be ignored. */
   onlyPublicRepositories? : boolean,
+  
+  /** Template files to use for the categories. */
   templateFiles? : TemplateFiles,
+  /**
+   * The category regex search pattern to use when searching for category labels in the repositories. Specify the
+   * categories as comma separated values in the label description.
+   */
   labelSearchPattern? : RegExp,
+
+  /** List of repository names to blacklist. */
   repositoryBlacklist? : string[],
+
+  /** Mappings between the category IDs and the category names. */
   categories? : Categories
 }
--- src/action/config/input/get_config.ts
+++ src/action/config/input/get_config.ts
@@ -5,11 +5,11 @@ import * as properties from './properties/index.ts'
 export function getConfig() : Partial<Config> {
 
   return {
+    organizationName: properties.organizationName(),
     onlyPublicRepositories: properties.onlyPublicRepositories(),
     templateFiles: properties.templateFiles(),
     labelSearchPattern: properties.labelSearchPattern(),
     repositoryBlacklist: properties.repositoryBlacklist(),
-    categories: properties.categories(),
-    organizationName: properties.organizationName()
+    categories: properties.categories()
   }
 }
